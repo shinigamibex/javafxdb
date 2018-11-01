058a02656c404838fbb0351aec68a4647389f68d
Aaron Angert <aangertdev@gmail.com>
2018-11-01 16:36:03 -0500

    first push

---
+++ chess_ml.py
@@ -0,0 +1,43 @@
+import os
+
+from enum import Enum
+class Piece(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+all_chess_games = []
+
+
+chess_game = []
+
+default_starting_board = {}
+current_board = {}
+player = True
+
+def player_move(move_string):
+    #at end
+    #if take move take_move()
+    #if castle do castle1()
+    player = !player
+
+def check_if_piece_exists(board,piece):
+    pass
+
+def check_piece_at_loc(board,location):
+    pass
+
+def move_piece(move, piece):
+    pass
+
+def take_piece():
+    pass
+
+def castle1():
+    pass
+
+def castle2():
+    pass
+
+def en_passan():
+    pass
