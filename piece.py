c003afee0a73e1a9070d5fcaccbb71d7a76dfde4
Andrew Bregger <adb3649@tamu.edu>
2018-11-01 18:40:45 -0500

    AB -- More complete move parsing

--- piece.py
+++ piece.py
@@ -1,38 +1,38 @@
 
 from enum import Enum
 class Piece(Enum):
-    Paw = 0
-    Pbw = 1
-    Pcw = 2
-    Pdw = 3
-    Pew = 4
-    Pfw = 5
-    Pgw = 6
-    Phw = 7
-    Pab = 8
-    Pbb = 9
-    Pcb = 10
-    Pdb = 11
-    Peb = 12
-    Pfb = 13
-    Pgb = 14
-    Phb = 15
-    Raw = 16
-    Rab = 17
-    Rbw = 18
-    Rbb = 19
+    P1w = 0
+    P2w = 1
+    P3w = 2
+    P4w = 3
+    P5w = 4
+    P6w = 5
+    P7w = 6
+    P8w = 7
+    P1b = 8
+    P2b = 9
+    P3b = 10
+    P4b = 11
+    P5b = 12
+    P6b = 13
+    P7b = 14
+    P8b = 15
+    R1w = 16
+    R2b = 17
+    R2w = 18
+    R2b = 19
     Kw = 20
     Kb = 21
-    Naw = 22
-    Nab = 23
-    Nbw = 24
-    Nbb = 25
+    N1w = 22
+    N1b = 23
+    N2w = 24
+    N2b = 25
     Qw = 26
     Qb = 27
-    Caw = 28
-    Cbw = 29
-    Cab = 30
-    Cbb = 31
+    C1w = 28
+    C2w = 29
+    C1b = 30
+    C2b = 31
 
 class GenericPiece(Enum):
     Pawn = 0
@@ -48,20 +48,46 @@ class MoveType(Enum):
     Castle1 = 2
     Castle2 = 3
     EnPassant = 4
+    PawnPromotion = 5
 
 class Move(object):
-    def __init__(self, ty, dest, start, piece):
+    def __init__(self, ty, dest, start, piece, promo = None):
         super().__init__()
         self.type = ty
         self.dest = dest
         self.start = start
         self.piece = piece
+        self.promotion = promo
 
     def __str__(self):
         return "{} {} {} {}".format(self.type, self.dest, self.start, self.piece)
 
+def get_piece(c):
+    return {'R': GenericPiece.Rook,
+            'C': GenericPiece.Castle,
+            'K': GenericPiece.King,
+            'Q': GenericPiece.Queen}[c]
 
 def parse_move_string(move_string):
+    if move_string == '0-0':
+        return Move(MoveType.Castle1, None, None, None)
+    elif move_string == '0-0-0':
+        return Move(MoveType.Castle2, None, None, None)
+
+    move_string = move_string.replace('+', '')
+    index = move_string.find('(ep)')
+    hasEn = False
+    promo = None
+    if index != -1:
+        # it is alway at the end
+        move_string = move_string.substr(index)
+        hasEn = True
+
+    if move_string[-1].isupper():
+        promo = get_piece(move_string[-1])
+        move_string = move_string[0:-2]
+
+
     piece = GenericPiece.Pawn
     column = None
     row = None
@@ -73,16 +99,7 @@ def parse_move_string(move_string):
     if move_string[0].isupper():
         p = move_string[0]
         move_string = move_string[1:]
-        if p == 'N':
-            piece = GenericPiece.Knight
-        elif p == 'R':
-            piece = GenericPiece.Rook
-        elif p == 'C':
-            piece = GenericPiece.Castle
-        elif p == 'K':
-            piece = GenericPiece.King
-        elif p == 'Q':
-            piece = GenericPiece.Queen
+        piece = get_piece(p)
 
     while len(move_string) > 2:
         if move_string[0].islower() or move_string[0].isdigit():
@@ -96,6 +113,10 @@ def parse_move_string(move_string):
                 moveType = MoveType.TakePiece
     dest = (int(move_string[1]), ord(move_string[0]) - ord('a') + 1)
     start = (column, row)
+    if hasEn:
+        moveType = MoveType.EnPassant
+    if not promo is None:
+        return Move(MoveType.PawnPromotion, dest, start, piece, promo)
     return Move(moveType, dest, start, piece)
 
 if __name__ == '__main__':
