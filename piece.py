7749445a752d3b205816f9334ba2fa8e7af7aa44
Aaron Angert <aangertdev@gmail.com>
2018-11-01 16:46:45 -0500

    updated files

---
+++ piece.py
@@ -0,0 +1,39 @@
+
+from enum import Enum
+class Piece(Enum):
+    aw = 1
+    bw = 2
+    cw = 3
+    dw = 3
+    ew = 3
+    fw = 3
+    gw = 3
+    hw = 3
+    ab = 1
+    bb = 2
+    cb = 3
+    db = 3
+    eb = 3
+    fb = 3
+    gb = 3
+    hb = 3
+    Raw = 3
+    Rab = 4
+    Rbw =3
+    Rbb = 6
+    Kw = 2
+    Kb = 3
+    Naw = 4
+    Nab = 4
+    Nbw = 4
+    Nbb = 4
+    Qw = 3
+    Qb = 3
+    Caw = 3
+    Cbw = 3
+    Cab = 4
+    Cbb = 6
+
+    
+def parse_move_string(move_string):
+    pass
