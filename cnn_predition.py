81d2371db9445f3073068c10f16d736cc2c3419d
Aaron Angert <aangertdev@gmail.com>
2018-11-17 16:11:28 -0600

    new commit

--- cnn_predition.py
+++ cnn_predition.py
@@ -6,13 +6,25 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import numpy as np
+import os
+import chess
+from dataset import *
+from game import *
 
-# batch_size = 128
-# num_classes = 10
-# epochs = 12
-#
-# # input image dimensions
-# img_rows, img_cols = 28, 28
-#
-# # the data, split between train and test sets
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+if __name__ == '__main__':
+    df = load_dataset()
+    game = parse_games(df)[0]
+
+    states = get_board_states(game, 5)
+    # print(states)
+
+    # print(get_board_state(game, 5))
+
+    full_board_image = []
+    for i in range(8):
+        for state in states:
+            for j in range(8):
+                full_board_image.append(state[(i*8)+j])
+    print(full_board_image)
+
+    
